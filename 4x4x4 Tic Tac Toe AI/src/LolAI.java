af55799bd46ecc1612ff905ac3e401bec7d01f68
Akshat Jain <respectthegame23@gmail.com>
2017-01-30 11:09:59 -0600

    lolAi

--- 4x4x4 Tic Tac Toe AI/src/LolAI.java
+++ 4x4x4 Tic Tac Toe AI/src/LolAI.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 import java.util.Random;
 
-public class Artificial_Stupidity_1 implements PlayerInt
+public class LolAI implements PlayerInt
 {
 	private char letter;
 	private String name;
 
 	// Pre:		receives a char letter
 	// Post: 	sets the name to "Random AI" and the letter to the letter received
-	Artificial_Stupidity_1(char letter)
+	LolAI(char letter)
 	{
-		name = "Artificial Stupidity 1";
+		name 		= "LolAI";
 		this.letter	= letter;
 	}
 
@@ -24,18 +24,18 @@ public class Artificial_Stupidity_1 implements PlayerInt
 	// Post: 	returns the Location where the player wants to move
 	public LocationInt getMove(BoardInt board)
 	{
+		Random rand = new Random();
 		ArrayList<Location> zeros = new ArrayList<Location>();
 		ArrayList<Location> singles = new ArrayList<Location>();
 		ArrayList<Location> doubles = new ArrayList<Location>();
 		ArrayList<Location> triples = new ArrayList<Location>();
 		ArrayList<Location> quadruples = new ArrayList<Location>();
-		
-		ArrayList<Location> otherZeros = new ArrayList<Location>();
-		ArrayList<Location> otherSingles = new ArrayList<Location>();
-		ArrayList<Location> otherDoubles = new ArrayList<Location>();
-		ArrayList<Location> otherTriples = new ArrayList<Location>();
-		ArrayList<Location> otherQuadruples = new ArrayList<Location>();
 
+		ArrayList<Location> ozeros = new ArrayList<Location>();
+		ArrayList<Location> osingles = new ArrayList<Location>();
+		ArrayList<Location> odoubles = new ArrayList<Location>();
+		ArrayList<Location> otriples = new ArrayList<Location>();
+		ArrayList<Location> oquadruples = new ArrayList<Location>();
 
 		for(int x = 0; x < board.numRows(); x++)
 			for(int y = 0; y < board.numRows(); y++)
@@ -75,27 +75,60 @@ public class Artificial_Stupidity_1 implements PlayerInt
 						LocationScore ls = new LocationScore(board,new Location(z,y,x),letter);
 						if(ls.getOtherQuadruples() >0)
 						{
-							otherQuadruples.add(new Location(z,y,x));
+							oquadruples.add(new Location(z,y,x));
 						}
 						else if(ls.getOtherTriples() >0)
 						{
-							otherTriples.add(new Location(z,y,x));
+							otriples.add(new Location(z,y,x));
 						}
 						else if(ls.getOtherDoubles() >0)
 						{
-							otherDoubles.add(new Location(z,y,x));
+							odoubles.add(new Location(z,y,x));
 						}
 						else if(ls.getOtherSingles() >0)
 						{
-							otherSingles.add(new Location(z,y,x));
+							osingles.add(new Location(z,y,x));
 						}
 						else
 						{
-							otherZeros.add(new Location(z,y,x));
+							ozeros.add(new Location(z,y,x));
 						}
 					}
 				}
-		return null;
+		if(quadruples.size() > 0)
+		{
+			return quadruples.get(rand.nextInt(quadruples.size()));
+		}
+		if(oquadruples.size()>0)
+		{
+			return oquadruples.get(rand.nextInt(oquadruples.size()));
+		}
+		//if(otriples.size()>0)
+		//{
+		//	return otriples.get(rand.nextInt(otriples.size()));
+	//	}
+		
+		else if(triples.size() > 0)
+		{
+			return triples.get(rand.nextInt(triples.size()));
+		}
+		
+		else if(doubles.size() > 0)
+		{
+			return doubles.get(rand.nextInt(doubles.size()));
+		}
+		else if(singles.size() > 0)
+		{
+			return singles.get(rand.nextInt(singles.size()));
+		}
+		else if(zeros.size() > 0)
+		{
+			return zeros.get(rand.nextInt(zeros.size()));
+		}
+		else
+		{
+			return new Location(0,0,0);
+		}
 	}
 
 	// Pre:		method is called
